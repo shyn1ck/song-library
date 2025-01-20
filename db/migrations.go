3bcc05806022d6255e15db26d3ba6c51aff7925e
shyn1ck <hy.parvizjon@gmail.com>
2025-01-20 13:47:42 +0500

    [feature]: Implement database migrations in migrations.go

--- db/migrations.go
+++ db/migrations.go
@@ -1,1 +1,27 @@
 package db
+
+import (
+	"errors"
+	"fmt"
+	"song-library/logger"
+)
+
+func Migrate() (err error) {
+	if dbConn == nil {
+
+		logger.Error.Printf("[db.Migrate]: database connection is not initialized: %v", err)
+		return errors.New("database connection is not initialized")
+
+	}
+
+	migrateModels := []interface{}{}
+
+	for _, model := range migrateModels {
+		err := dbConn.AutoMigrate(model)
+		if err != nil {
+			return fmt.Errorf("failed to migrate %T: %v", model, err)
+		}
+	}
+
+	return nil
+}
