38374d9a48c7e98796bedab6f74d74db533d76b6
shyn1ck <hy.parvizjon@gmail.com>
2025-01-21 15:59:13 +0500

    [refactor] Update song service layer

--- pkg/services/songs.go
+++ pkg/services/songs.go
@@ -1,24 +1,17 @@
 package service
 
 import (
-	"errors"
 	"song-library/models"
 	"song-library/pkg/repository"
+	"song-library/utils"
 )
 
-func GetSongs(group, song string, page, limit int) ([]models.Song, error) {
-	if group == "" && song == "" {
-		return nil, errors.New("at least one of 'group' or 'song' must be provided")
+func GetSongs(group, song string, page, limit int) (songs []models.Song, err error) {
+	if page <= 0 || limit <= 0 || limit > 100 {
+		return nil, utils.ErrInvalidPaginationParams
 	}
 
-	if page <= 0 {
-		return nil, errors.New("the 'page' parameter must be greater than 0")
-	}
-	if limit <= 0 || limit > 100 {
-		return nil, errors.New("the 'limit' parameter must be between 1 and 100")
-	}
-
-	songs, err := repository.GetSongs(group, song, page, limit)
+	songs, err = repository.GetSongs(group, song, page, limit)
 	if err != nil {
 		return nil, err
 	}
