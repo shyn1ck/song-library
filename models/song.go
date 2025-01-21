7377bdbb16eca01092bf396ea2f7806746fce7ce
shyn1ck <hy.parvizjon@gmail.com>
2025-01-21 15:59:12 +0500

    [refactor] Modify song model structure

--- models/song.go
+++ models/song.go
@@ -1,10 +1,15 @@
 package models
 
+import "time"
+
 type Song struct {
-	ID          uint   `json:"id" gorm:"primaryKey"`
-	Group       string `json:"group"`
-	SongTitle   string `json:"song"`
-	ReleaseDate string `json:"releaseDate"`
-	Text        string `json:"text"`
-	Link        string `json:"link"`
+	ID          uint       `gorm:"primaryKey" json:"id"`
+	Group       string     `json:"group"`
+	Song        string     `json:"song"`
+	ReleaseDate string     `json:"release_date"`
+	Text        string     `json:"text"`
+	Link        string     `json:"link"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	DeletedAt   *time.Time `gorm:"index" json:"deleted_at,omitempty"`
 }
