750724df85aa22693f1f41a1f9248975a6e6a88b
shyn1ck <hy.parvizjon@gmail.com>
2025-01-21 22:59:27 +0500

    [update]: delete superfluous errs

--- utils/errs.go
+++ utils/errs.go
@@ -25,5 +25,4 @@ var (
 	ErrInvalidRequestBody           = errors.New("ErrInvalidRequestBody")
 	ErrInvalidRequestParameter      = errors.New("ErrInvalidRequestParameter")
 	ErrGroupNotFound                = errors.New("ErrGroupNotFound")
-	ErrSongNotFound                 = errors.New("ErrSongNotFound")
 )
