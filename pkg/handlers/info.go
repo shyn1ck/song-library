cd1544df48de4aa74599f1eec0dc08b2314387ab
shyn1ck <hy.parvizjon@gmail.com>
2025-01-21 22:23:20 +0500

    [feature]: Add new handler for API

--- pkg/handlers/info.go
+++ pkg/handlers/info.go
@@ -1,1 +1,31 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"song-library/logger"
+	services "song-library/pkg/services"
+	"song-library/utils"
+)
+
+func ApiInfo(c *gin.Context) {
+	ip := c.ClientIP()
+	logger.Info.Printf("[handlers.ApiInfo]: Client with ip: %s request to get InfoSong", ip)
+
+	group := c.Query("group")
+	song := c.Query("song")
+
+	if group == "" || song == "" {
+		logger.Error.Printf("[handlers.ApiInfo]: Error Invalid Request Parameter")
+		handleError(c, utils.ErrInvalidRequestParameter)
+		return
+	}
+
+	songDetail, err := services.GetSongDetail(group, song)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+	logger.Info.Printf("[handlers.ApiInfo]: Client with ip: %s, successufly to get InfoSong", ip)
+	c.JSON(http.StatusOK, songDetail)
+}
