f76d2a392a83676f356acff021f2c9c1d8778f01
shyn1ck <hy.parvizjon@gmail.com>
2025-01-21 15:59:13 +0500

    [refactor] Update song repository functions

--- pkg/repository/songs.go
+++ pkg/repository/songs.go
@@ -1,13 +1,37 @@
 package repository
 
 import (
+	"errors"
+	"gorm.io/gorm"
 	"song-library/db"
 	"song-library/models"
+	"song-library/utils"
 )
 
-func GetSongs(group string, song string, page int, limit int) (songs []models.Song, err error) {
+func GetSongs(group, song string, page, limit int) ([]models.Song, error) {
+	var songs []models.Song
+	offset := (page - 1) * limit
 
-	err = db.GetDBConn().Model(&songs).Find(&songs).Error
+	query := db.GetDBConn().Model(&songs)
 
-	return songs, err
+	if group != "" {
+		query = query.Where("\"group\" = ?", group)
+	}
+	if song != "" {
+		query = query.Where("song = ?", song)
+	}
+
+	err := query.Offset(offset).Limit(limit).Find(&songs).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, utils.ErrDatabaseConnectionFailed
+	}
+
+	if len(songs) == 0 {
+		return nil, nil
+	}
+
+	return songs, nil
 }
