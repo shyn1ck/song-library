78f4413787a87d7042b41f5f347776ed2d2aac5e
shyn1ck <hy.parvizjon@gmail.com>
2025-01-22 11:42:52 +0500

    [update]: Added new custom errs ErrAPIRequestFailed

--- utils/errs.go
+++ utils/errs.go
@@ -25,4 +25,5 @@ var (
 	ErrInvalidRequestBody           = errors.New("ErrInvalidRequestBody")
 	ErrInvalidRequestParameter      = errors.New("ErrInvalidRequestParameter")
 	ErrGroupNotFound                = errors.New("ErrGroupNotFound")
+	ErrAPIRequestFailed             = errors.New("ErrAPIRequestFailed")
 )
